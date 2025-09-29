bd297e8e754940c11bed1715ecbb20d22e64b014
Shreyash Tailor <tailorshreyash01@gmail.com>
2025-09-29 13:48:45 +0530

    Fix database connection parameters in db.php

--- db.php
+++ db.php
@@ -1,12 +1,11 @@
 <?php
-$host = "db.fr-pari1.bengt.wasmernet.com";
-$port=10272;
-$name = "dbGFjXeeTfzoBSkj45iZMzKb";
+$host="db.fr-pari1.bengt.wasmernet.com";
+$port = (int)$portString;
 $username='a3f228567afe80000576f53a820d';
 $pass = "068da3f2-2856-7c5a-8000-cff47e0a369f";
 $db = "dbGFjXeeTfzoBSkj45iZMzKb";
 
-$conn = mysqli_connect($host, $port, $name, $username, $pass, $db);
+$conn = mysqli_connect($host, $username, $password, $db, $port);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
