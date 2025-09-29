bfcd4c08cf10815f1d78936eb020905665b744bc
Shreyash Tailor <tailorshreyash01@gmail.com>
2025-09-29 13:45:25 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "quiz";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$port=10272;
+$name = "dbGFjXeeTfzoBSkj45iZMzKb";
+$username='a3f228567afe80000576f53a820d';
+$pass = "068da3f2-2856-7c5a-8000-cff47e0a369f";
+$db = "dbGFjXeeTfzoBSkj45iZMzKb";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $port, $name, $username, $pass, $db);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
