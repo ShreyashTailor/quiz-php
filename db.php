1ccd42a3fe1063265cd94c1ec2f96c38ef8d6109
Shreyash Tailor <tailorshreyash01@gmail.com>
2025-09-29 13:56:55 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host="db.fr-pari1.bengt.wasmernet.com";
-$port = (int)$portString;
-$username='a3f228567afe80000576f53a820d';
-$pass = $_ENV['pass'];
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$port = 10272; 
+$username = "a3f228567afe80000576f53a820d";
+$pass = "068da3f2-2856-7c5a-8000-cff47e0a369f";
 $db = "dbGFjXeeTfzoBSkj45iZMzKb";
 
 $conn = mysqli_connect($host, $username, $pass, $db, $port);
