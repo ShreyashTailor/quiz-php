e37ef5fc95d0d33d9793dce88f56fc7ea5738839
Shreyash Tailor <tailorshreyash01@gmail.com>
2025-09-29 13:32:13 +0530

    Delete file.php

--- file.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$fileinput=readline('Enter file content:');
-file_put_contents('file.txt',$fileinput);
-echo "File created successfully.";
-//read
-$content=file_get_contents('file.txt');
-echo "\nFile content: $content";
